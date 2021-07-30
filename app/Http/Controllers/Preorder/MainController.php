203eb5b35db713f79dbafa3a00dd8acfe2712791
qaweeds <qaweeds@gmail.com>
2021-07-30 17:09:22 +0300

    add exchange model m add deliverytime model m

--- app/Http/Controllers/Preorder/MainController.php
+++ app/Http/Controllers/Preorder/MainController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Preorder;
 
 use App\Http\Controllers\Controller;
 use App\Models\Preorder\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
