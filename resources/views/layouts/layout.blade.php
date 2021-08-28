5c41a3fa622e7dff8e0ea8d6c45e9618731c9f8b
qaweeds <qaweeds@gmail.com>
2021-08-28 10:17:19 +0300

    finish21

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,8 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=noshrink-to-fit=no">
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/main.css">
     @yield('styles')
